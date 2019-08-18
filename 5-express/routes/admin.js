b115be5eef286ddb85c183a4c5525badcea4b7f3
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-08-19 00:52:04 +0200

    section 6 done
    Working with Dynamic Content
    & Adding Templating Engines

--- 5-express/routes/admin.js
+++ 5-express/routes/admin.js
@@ -3,16 +3,24 @@ const express = require("express");
 const router = express.Router();
 const rootDir = require("../util/path");
 
+const products = [];
+
 router.get("/add-product", (req, res, next) => {
     //console.log("The add-product page!");
     //res.send(`<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type"submit">Add product!</button></form>`);
-    res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+    res.render("add-product", {
+        pageTitle: "Add Product", 
+        path: "/admin/add-product", 
+        formsCSS: true, 
+        productCSS: true, 
+        activeAddProduct: true })
 });
 
 router.post("/add-product", (req, res) => {
-    console.log(req.body);
+    products.push({ title: req.body.title });
     res.redirect("/");
 });
 
-
-module.exports = router;
+exports.routes = router;
+exports.products = products;
---
+++ 5-express/routes/shop.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const express = require("express");
+const rootDir = require("../util/path")
+const router = express.Router();
+const adminData = require("./admin");
+
+router.get("/", (req, res, next) => {
+    //console.log("In another middleware!");
+    //res.send("<h1>Hello from express!</h1>");
+    // console.log('shop.js', adminData.products);
+    // res.sendFile(path.join(rootDir , "views", "shop.html"));
+
+    const products = adminData.products;
+    res.render("shop", {
+        prods: products, 
+        pageTitle: "Shop", 
+        path: "/", 
+        hasProducts: products.length > 0, 
+        activeShop: true,
+        productCSS: true
+    });
+});
+
+module.exports = router;
