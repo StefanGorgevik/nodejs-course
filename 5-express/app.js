f95cec74889b8195d938b806a107092d4641a034
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-08-19 16:55:53 +0200

    MVC finished
    Section 7: The Model View Controller
    (MVC)

--- 5-express/app.js
+++ 5-express/app.js
@@ -1,43 +1,26 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
-//const hbs = require("express-handlebars");
+const pageNotFound = require("./controllers/page-not-found");
 
 const app = express();
 
-//app.engine("hbs", hbs({layoutsDir: "views/layouts/", defaultLayout: "main-layout", extname: "hbs"}));
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminData = require("./routes/admin");
+const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
-
-app.get("*", (req, res) => {
-    //res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-    res.status(404).render("404", {pageTitle: "Page Not Found"});
-});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin",adminData.routes);
+app.use("/admin",adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render("404", {pageTitle: "Page Not Found"});
-});
-
+app.use(pageNotFound.get404);
 
 app.listen(3000, (err) => {
     if(err) return console.log(err);
     return console.log("Server has started successfully!");
 })
-                // // app.use((req, res, next) => {
-                // //     console.log("In the middleware!");
-                // //     next(); //allows the req to continue to the next middleware in line
-                // // });
-                
-                // app.use("/", (req, res, next) => {
-                //     //console.log("This will always run!!");
-                //     next();
-                // });
+               
--- 5-express/routes/admin.js
+++ 5-express/routes/admin.js
@@ -1,26 +1,10 @@
 const path = require("path");
 const express = require("express");
 const router = express.Router();
-const rootDir = require("../util/path");
+const productsController = require("../controllers/products");
 
-const products = [];
+router.get("/add-product", productsController.getAddProduct);
 
-router.get("/add-product", (req, res, next) => {
-    //console.log("The add-product page!");
-    //res.send(`<form action="/admin/add-product" method="POST"><input name="title" type="text"><button type"submit">Add product!</button></form>`);
-    //res.sendFile(path.join(rootDir, "views", "add-product.html"));
-    res.render("add-product", {
-        pageTitle: "Add Product", 
-        path: "/admin/add-product", 
-        formsCSS: true, 
-        productCSS: true, 
-        activeAddProduct: true })
-});
+router.post("/add-product", productsController.postAddProduct);
 
-router.post("/add-product", (req, res) => {
-    products.push({ title: req.body.title });
-    res.redirect("/");
-});
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
