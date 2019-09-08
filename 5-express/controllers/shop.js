0e507b4a8e0548fc322c34f0a88c2b85904aa1f2
Stefan Gorgevik <stefangorgevik@hotmail.com>
2019-09-08 22:31:16 +0200

    updates on details of product

--- 5-express/controllers/shop.js
+++ 5-express/controllers/shop.js
@@ -11,6 +11,17 @@ exports.getProducts = (req, res, next) => {
     });
 };
 
+exports.getProduct = (req, res) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        res.render("shop/product-detail", {
+            product: product,
+            pageTitle: product.title,
+            path: "/products"
+        });
+    });
+}
+
 exports.getIndex = (req, res) => {
     Product.fetchAll((products) => {
         res.render("shop/index", {
